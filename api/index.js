c8664b7ec53357feb22497466f2e9667ff9d415a
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-11-15 08:22:30 +0800

    first commit

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import { sql } from "@vercel/postgres";
 
 export default async function handler(req, res) {
   try {
-    const { rows } = await sql`SELECT * FROM qrdata;`
+    const { rows } = await sql`SELECT * FROM qrdata`;
     res.status(200).json(rows);
   } catch (error) {
     console.error('Database query error:', error);
